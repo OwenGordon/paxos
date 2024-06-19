aeb406880f6104595f0cc202d4c54b521e35b439
jonah slack <jcsslack@gmail.com>
2024-06-19 16:20:10 -0400

    simple log server

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ deploy:
 build:
 	docker build -t db-node-app:latest ./db-node
 	docker build -t master-node-app:latest ./master
+	docker build -t log-server-app:latest ./log-server
 
 clean:
 	docker stack rm paxos
---
+++ log-server/src/main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, Depends, Response
+from uuid import uuid4
+from pydantic import BaseModel
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import sessionmaker
+
+# Create an in-memory database engine
+engine = create_engine("sqlite:///:memory:", echo=True, connect_args={"check_same_thread": False})
+
+# Create a sessionmaker factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a FastAPI app
+app = FastAPI()
+
+# Create a dependency to get a database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Define a base model
+class Log(BaseModel):
+    timestamp: str
+    source: str
+    destination: str
+    action: str
+
+
+Base = declarative_base()
+class DBItem(Base):
+    __tablename__ = 'items'
+    id = Column(String, primary_key=True, index=True)
+    timestamp = Column(String, index=True)
+    source = Column(String, index=True)
+    destination = Column(String, index=True)
+    action = Column(String, index=True)
+Base.metadata.create_all(bind=engine)
+
+class LogResponse(Log):
+    id: str
+
+# Create an endpoint to create an item
+@app.post("/log/")
+async def create_log(log: Log, db = Depends(get_db)):
+    id = str(uuid4())
+    dbitem: DBItem = DBItem(timestamp=log.timestamp, source=log.source, destination=log.destination, action=log.action, id=id)
+    db.add(dbitem)
+    db.commit()
+    db.refresh(dbitem)
+    return Response(status_code=200, content=f"Created item with id: {id}")
+
+# Create an endpoint to get all items
+@app.get("/logs/", response_model=list[LogResponse])
+async def read_items(db = Depends(get_db)):
+    raw_items: list[DBItem] = db.query(DBItem).all()
+    logs = [LogResponse(timestamp=item.timestamp, source=item.source, action=item.action, destination=item.destination, id=item.id) for item in raw_items]
+    return logs
